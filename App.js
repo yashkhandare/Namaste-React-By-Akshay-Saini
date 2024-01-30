9b3c2e4ebf28496eed1cc38abfea2683e44b352e
Yash Ramesh Khandare <yashkumarkhandare@gmail.com>
2024-01-30 12:53:17 +0530

    First Code commit on 30th Jan

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+
 const parent = React.createElement("div", { id: "parent"} ,[
     React.createElement("div", { id: "child"}, [
         React.createElement("h1", {}, "im h1 Tag"),
@@ -10,11 +14,8 @@ const parent = React.createElement("div", { id: "parent"} ,[
 ]),
 ]);
 
+console.log(parent);
 
+const root  = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
-
-
-
-),
+root.render(parent);
