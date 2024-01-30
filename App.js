98df46bfb22afbe0cc5992ae044f70ea81dc0534
Yash Ramesh Khandare <yashkumarkhandare@gmail.com>
2024-01-30 09:37:25 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,20 @@
+const parent = React.createElement("div", { id: "parent"} ,[
+    React.createElement("div", { id: "child"}, [
+        React.createElement("h1", {}, "im h1 Tag"),
+        React.createElement("h2", {}, "Im h2 tag"),
+    ]),
+
+    React.createElement("div", { id:"child2"}, [
+        React.createElement("h1", {}, "Im h1 tag"),
+        React.createElement("h2", {}, "im h2 tag"),
+]),
+]);
+
+
+
+
+
+
+
+
+),
