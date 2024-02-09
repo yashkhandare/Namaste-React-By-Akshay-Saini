b214164a77c9934430a67c08d427791cf2e944be
Yash Ramesh Khandare <yashkumarkhandare@gmail.com>
2024-02-09 22:16:59 +0530

    App.js has 2 code, both are same

--- App.js
+++ App.js
@@ -2,3 +2,10 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 
 const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+
+
+const newheading = () => <h1>This is without Braces Functional Comonent.</h1>;
+
+const newheading2 = () => { 
+            <h1>This is with Braces Functional Comonent.</h1>
+        };
