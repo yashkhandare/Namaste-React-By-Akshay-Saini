4a79de3f1c6cd044b7c79e6f6c3c77fff670dcef
Yash Ramesh Khandare <yashkumarkhandare@gmail.com>
2024-02-09 22:03:28 +0530

    Newly added react

--- App.js
+++ App.js
@@ -1,14 +1,4 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-<<<<<<< HEAD
 const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
-
-const jsxheading = <h1 id="heading">Hi Yash This msg is from JSX.</h1>
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxheading);
-=======
-const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
->>>>>>> 10d530d03a6a60d9685e9d76f698ab8d0234edb7
